74b8831794138f7a37a13b1edaa83843efe0c32a
michael pei <382830381@qq.com>
2017-09-15 21:29:49 +0800

    增加内存池文件

--- CMemPool/CMemPool.cpp
+++ CMemPool/CMemPool.cpp
@@ -2,9 +2,138 @@
  * CMemPool.cpp
  *
  *  Created on: 2017年9月13日
- *      Author: micheal
+ *      Author: michael
  */
 
-
 #include "CMemPool.h"
+#include <iostream>
+BSL::MemBlock::MemBlock (size_t nSize):
+pStart(nullptr)
+,pEnd(nullptr)
+,nBlockSplices(NBLOCKSPLICES)
+,nSpliceSize(0)
+{
+    std::cout << "malloc\n";
+    pStart = malloc(nSize*NBLOCKSPLICES);
+    pEnd = (char*)pStart + nSize*NBLOCKSPLICES;
+    nSpliceSize = nSize;
+    vecpSplices.resize(NBLOCKSPLICES);
+    for(int i=0;i<NBLOCKSPLICES;++i)
+    {
+        vecpSplices[i] = pStart + i * nSpliceSize;
+    }
+}
 
+void*
+BSL::MemBlock::FetchSplice (size_t nSize)
+{
+    int index = nFetchIndex++;
+    void* retPtr = nullptr;
+    auto curSplice = vecpSplices[index%nBlockSplices];
+    if(curSplice != nullptr)
+    {
+        retPtr = curSplice;
+        vecpSplices[index%nBlockSplices] = nullptr;
+    }
+    return retPtr;
+}
+
+void
+BSL::MemBlock::RecoverSplice (void* pSplice)
+{
+    int index = nReturnIndex++;
+    auto& curSplice = vecpSplices[index%nBlockSplices];
+    if(curSplice == nullptr)
+    {
+        curSplice = pSplice;
+    }
+}
+
+BSL::CMemPool::CMemPool ()
+{
+    Init();
+}
+
+BSL::CMemPool::~CMemPool ()
+{
+}
+
+void*
+BSL::CMemPool::alloc (size_t nSize)
+{
+
+    for(auto v:m_vecpBlocks)
+    {
+        if(v->nSpliceSize >= nSize)
+        {
+            auto retptr = v->FetchSplice(nSize);
+            if(retptr != nullptr)
+                return retptr;
+        }
+    }
+    //没有合适的内存块，则创建新的内存块
+    int nExist = 0;
+    for(auto v:DEFALUT_SPLICE)
+    {
+        if(v >= nSize)
+            nExist = v;
+    }
+    return Create(nSize);
+
+}
+
+void
+BSL::CMemPool::dealloc (void* pSplice)
+{
+    for(auto v:m_vecpBlocks)
+    {
+        if(v->pStart <= pSplice && v->pEnd > pSplice)
+        {
+            v->RecoverSplice(pSplice);
+            return;
+        }
+    }
+}
+
+void
+BSL::CMemPool::Init (void)
+{
+    Create(DEFALUT_SPLICE[7]);
+}
+
+void*
+BSL::CMemPool::Create (size_t nSize)
+{
+    MemBlock* pBlock = new MemBlock(nSize);
+    EnlargePool(pBlock);
+    return pBlock;
+}
+
+void
+BSL::CMemPool::EnlargePool (void* pBlock)
+{
+    if(nullptr == pBlock)
+        return ;
+    std::unique_lock<std::mutex> lock(m_Mutex);
+    m_vecpBlocks.push_back(reinterpret_cast<MemBlock*>(pBlock));
+}
+
+void
+BSL::CMemPool::ReleaseSplice (void* pSplice)
+{
+    free(pSplice);
+}
+
+void
+BSL::CMemPool::ReleaseBlock (void* pBlock)
+{
+    for(auto v:m_vecpBlocks)
+    {
+        for(auto splice:v->vecpSplices)
+        {
+            ReleaseSplice(splice);
+        }
+        delete v;
+        v = NULL;
+    }
+}
--- CMemPool/CMemPool.h
+++ CMemPool/CMemPool.h
@@ -2,13 +2,90 @@
  * CMemPool.h
  *
  *  Created on: 2017年9月13日
- *      Author: micheal
+ *      Author: michael
  */
 
 #ifndef CMEMPOOL_H_
 #define CMEMPOOL_H_
+/**
+ * c header
+ */
+#include <stdint.h>
+#include <stdlib.h>
+/**
+ * c++ header
+ */
+#include <atomic>
+#include <vector>
+#include <mutex>
+
+const static int NBLOCKSPLICES = 2;
+namespace BSL
+{
+
+    const static int DEFALUT_SPLICE[8] = {16,32,64,128,256,512,1024,2048};
+
+    struct MemBlock{
+        void* pStart;///<内存块开始地址
+        void* pEnd;///<内存块结束地址
+        const uint8_t nBlockSplices;///内存块分片数量
+        uint16_t nSpliceSize;///<内存块分片大小
+        std::atomic_int nFetchIndex;///<分配内存片原子计数
+        std::atomic_int nReturnIndex;///<回收内存片原子计数
+        std::vector<void*> vecpSplices;///<管理分片容器
+        MemBlock(size_t nSize);
+        void* FetchSplice(size_t nSize);
+        void RecoverSplice(void* pSplice);
+    };
+
+    class CMemPool
+    {
+    public:
+        CMemPool();
+        ~CMemPool();
+    public:
+        /**
+         * @brief GetMem 获取内存块
+         * @param nSize 大小
+         * @return void* 内存指针
+         */
+        void* alloc(size_t nSize);
+        /**
+         * @brief ReturnMem 归还内存块
+         * @param pSplice 内存指针
+         */
+        void dealloc(void* pSplice);
+        /**
+         * @brief ReleaseBlock 释放内存块
+         * @param pBlock 内存指针
+         */
+        void ReleaseBlock(void* pBlock);
+    private:
+
+        void Init(void);
+        /**
+         * @brief Create 创建指定大小的内存块
+         * @param nSize 指定的大小
+         */
+        void* Create(size_t nSize);
+        /**
+         * @brief EnlargePool 扩充内存池容量
+         * @param pBlock 内存指针
+         */
+        void EnlargePool(void* pBlock);
+        /**
+         * @brief ReleaseSplice 释放内存片
+         * @param pSplice 内存指针
+         */
+        void ReleaseSplice(void* pSplice);
 
 
+    private:
+        std::mutex m_Mutex;
+        std::vector<MemBlock*> m_vecpBlocks;
+    };
+
+}//namespace BSL
 
 
 
